2c950dd5e613b1d5f2c7c5fc564ee5601929296b
ahondjo kwonjin <k.ahondjo98@gmail.com>
2025-06-19 09:42:12 +0000

    chance frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import ProductDetail from './pages/ProductDetail';
-import Cart from './pages/Cart';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 // import Register from './pages/Register';
@@ -44,9 +43,8 @@ function AppLayout() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/products/:id" element={<ProductDetail />} /> */}
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
           {/* <Route path="/register" element={<Register />} /> */}
         </Routes>
